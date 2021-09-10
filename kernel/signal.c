1aeaf6f68618897203f510d893874522087472db
Cyber Knight <cyberknight755@gmail.com>
2021-09-11 02:29:51 +0800

    Revert "mm: oom_kill: reap memory of a task that receives SIGKILL"
    The commit: 724adb443397221ef25afee5183466aa9ae49703 hurts performance
    slightly as per Sultan and we are not using the oom reaper with SLMK so
    it is pretty much of no use.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -42,8 +42,7 @@
 #include <linux/cn_proc.h>
 #include <linux/compiler.h>
 #include <linux/posix-timers.h>
-#include <linux/oom.h>
-#include <linux/capability.h>
+#include <linux/cgroup.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/signal.h>
@@ -1344,11 +1343,8 @@ int group_send_sig_info(int sig, struct siginfo *info, struct task_struct *p)
 	ret = check_kill_permission(sig, info, p);
 	rcu_read_unlock();
 
-	if (!ret && sig) {
+	if (!ret && sig)
 		ret = do_send_sig_info(sig, info, p, true);
-		if (capable(CAP_KILL) && sig == SIGKILL)
-			add_to_oom_reaper(p);
-	}
 
 	return ret;
 }
