000b051436d3cec4057d3fa0a0a108dc9524bdd2
dopaemon <polarisdp@gmail.com>
2023-06-04 10:13:31 +0700

    fs: ksu: error: undefined symbol
    ld.lld: error: undefined symbol: ksu_handle_faccessat
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(sys_faccessat)
    
    ld.lld: error: undefined symbol: ksu_handle_vfs_read
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(vfs_read)
    
    ld.lld: error: undefined symbol: ksu_handle_stat
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(vfs_statx)
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(SyS_newstat)
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(SyS_newlstat)
    >>> referenced 4 more times
    
    ld.lld: error: undefined symbol: ksu_handle_execveat
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(do_execveat_common)
    
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1704,8 +1704,10 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 // KernelSU hook
+#ifdef CONFIG_KSU
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			       void *envp, int *flags);
+#endif
 
 /*
  * sys_execve() executes a new program.
@@ -1721,7 +1723,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+#ifdef CONFIG_KSU
         ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);  // call KSU hook first
+#endif
+
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
